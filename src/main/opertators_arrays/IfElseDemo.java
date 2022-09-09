eb00e2e3231d9e8f0050b96ad318d6985114f3fe
Timur <timk01@mail.ru>
2022-09-09 17:12:11 +0300

    practise anagramm

---
+++ src/main/opertators_arrays/IfElseDemo.java
@@ -0,0 +1,14 @@
+package opertators_arrays;
+
+import java.util.Scanner;
+
+public class IfElseDemo {
+    public static void main(String[] args) {
+        System.out.println("Завтра отдыхаем? (1 - да, 0 - нет");
+        Scanner sc = new Scanner("System.in");
+        if (sc.hasNextInt()) {
+            String str = sc.nextInt() == 1 ? "ура" : "не ура";
+            System.out.println(str);
+        }
+    }
+}
